937d1f74ab93d02859ca158ad898d71d8375c59c
KevinKarin06 <kevinkarin06@gmail.com>
2021-09-22 07:00:19 +0100

    user user info component and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
+import { LinkComponent } from './components/link/link.component';
+import { MessageComponent } from './components/message/message.component';
+import { MessageListComponent } from './components/message-list/message-list.component';
+import { MessageBodyComponent } from './components/message-body/message-body.component';
+import { MessageListItemComponent } from './components/message-list-item/message-list-item.component';
+import { ButtonComponent } from './components/button/button.component';
+import { NewMessageComponent } from './components/new-message/new-message.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AvatarComponent,
+    LinkComponent,
+    MessageComponent,
+    MessageListComponent,
+    MessageBodyComponent,
+    MessageListItemComponent,
+    ButtonComponent,
+    NewMessageComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
